adb5dfb1619a59e5a3913429dc817afc52f6c6f7
Sun, Yi <sunyi@xiaokr.cc>
2016-07-22 08:17:04 +0000

    Seperate X,Y,Z axis.
    Signed-off-by: Sun, Yi <sunyi@xiaokr.cc>

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -3,72 +3,59 @@
 #define MAX_QUEUE	512
 #define CMD_SIZE	(5 * 4)
 
-byte pin_x=3;              //x方向电机驱动脉冲端口
-byte pin_y=5;             //y方向电机机驱动脉冲端口
-byte pin_z=9;            //z方向电机机驱动脉冲端口
+#define PIN_PULS 5
+#define PIN_DIR 6
+#define PIN_ENABLE 8
 
+#define PIN_INTER 3          
+#define PIN_INTER_RIGHT 9    
+#define PIN_INTER_LEFT 10   
 
-byte dir_x=4;            //x方向电机方向端口
-byte dir_y=6;           //y方向电机方向端口
-byte dir_z=10;          //z方向电机方向端口
+#define TRAN_RATION_X 95
+#define TRAN_RATION_Y 75
+#define TRAN_RATION_Z 95
+#define DELAY_TF_X 200
+#define DELAY_TF_Y 400
+#define DELAY_TF_Z 200
 
-byte enable_x=7;          //x方向电机使能端口
-byte enable_y=8;         //y方向电机使能端口
-byte enable_z=11;       //z方向电机使能端口
+#define PULSE_RATE_X 800
+#define PULSE_RATE_Y 400
+#define PULSE_RATE_Z 800
 
-//用EEPROM寄存器保存坐标地址
-byte address_x_HIGH=1;
-byte address_x_LOW=2;
-byte address_y_HIGH=3;
-byte address_y_LOW=4;
-byte address_z_HIGH=5;
-byte address_z_LOW=6;
+#define ADDRESS_LOW 2
+#define ADDRESS_HIGH 1
 
+#define ACC_TIME 10000            //acclebrate 10ms
+#define MAX_DELAY 1000
+
+#define SORT 'y'
 //EEPROM寄存器保存的高低位数
-int value_x_high;
-int value_x_low;
-int value_y_high;
-int value_y_low;
-int value_z_high;
-int value_z_low;
+int value_high;
+int value_low;
 int value_time_0;
 int value_time_1;
 int value_time_2;
 int value_time_3;
 
 //电机参数
-int objpos_x,objpos_y,objpos_z;           //x，y，z方向目标位置坐标
-int curpos_x,curpos_y,curpos_z;          //x，y，z方向当前目标坐标
-int distance_x,distance_y,distance_z;   //x，y，z方向所需移动位移
-
-//位移与角度转化比
-int DISSWANG_X=95;                      //50轨道
-int DISSWANG_Y=75;                      //45轨道
-int DISSWANG_Z=95;                     //50轨道
-int pulse_rate_x = 800;                //x方向电机旋转一圈所需脉冲数
-int pulse_rate_y = 220;                //y方向电机旋转一圈所需脉冲数
-int pulse_rate_z = 400;              //z方向电机旋转一圈所需脉冲数
-
-int delay_lowlevenum_xlow=15;          //x方向电机低速低电平脉冲延迟数
-int delay_lowlevenum_xmid=15;          //x方向电机低速中电平脉冲延迟数
-int delay_lowlevenum_xhig=15;          //x方向电机低速高电平脉冲延迟数
-int delay_lowlevenum_ylow=30;          //y方向电机低速低电平脉冲延迟数
-int delay_lowlevenum_ymid=30;          //y方向电机低速中电平脉冲延迟数
-int delay_lowlevenum_yhig=30;          //y方向电机低速高电平脉冲延迟数
-int delay_lowlevenum_zlow=60;          //z方向电机低速低电平脉冲延迟数
-int delay_lowlevenum_zmid=60;          //z方向电机低速中电平脉冲延迟数
-int delay_lowlevenum_zhig=60;          //z方向电机低速高电平脉冲延迟数
+int objpos_coor;           //target pos
+int curpos_coor;          //curret pos
+int distance_coor;       //distance of moving
 
 
-//串口数据
-int string_head = 0;                                //数据头指针地址
-int string_end = 0;                                 //数据尾指针地址      
-unsigned char * comdata;                           //读取串口数据                                
+boolean inter_stop = false;
 
-//获取机器运行的时间长度, 单位毫秒
+//pointer of serial buffer
+int string_head = 0;
+int string_end = 0;
+unsigned char * comdata;
+
+//run duration of last running
 unsigned long motor_time;
 unsigned long motor_time_head;
 unsigned long motor_time_end;
+
+//register for time calculation
 byte address_time_0=7;
 byte address_time_1=8;
 byte address_time_2=9;
@@ -96,18 +83,45 @@ union CMD
 
 bool g_processFlag = false;
 
+void stop_motor()
+{
+    digitalWrite(PIN_PULS,LOW);
+    digitalWrite(PIN_ENABLE,HIGH);
+    Serial.println("stop_motor:");
+}
+
+//function for interrupt
+void blink()
+{
+     stop_motor();
+     
+     inter_stop = true;
+     int val_right = digitalRead(PIN_INTER_RIGHT);
+     int val_left = digitalRead(PIN_INTER_LEFT);
+     if(val_right)
+     {
+         digitalWrite(PIN_DIR, HIGH);
+         return;
+     }
+     if(val_left)
+     {
+         digitalWrite(PIN_DIR, LOW);
+         return;
+     }
+     Serial.println(" blink:");
+}
+
 void setup() {
 	// put your setup code here, to run once:
 	//---===引脚模式设置===---// 
-	pinMode(pin_x,OUTPUT);
-	pinMode(pin_y,OUTPUT);
-	pinMode(pin_z,OUTPUT);
-	pinMode(dir_x,OUTPUT);
-	pinMode(dir_y,OUTPUT);
-	pinMode(dir_z,OUTPUT);
-	pinMode(enable_x,OUTPUT);
-	pinMode(enable_y,OUTPUT);
-	pinMode(enable_z,OUTPUT);
+	pinMode(PIN_PULS,OUTPUT);
+	pinMode(PIN_DIR,OUTPUT);
+	pinMode(PIN_ENABLE,OUTPUT);
+
+	pinMode(PIN_INTER,OUTPUT);
+	pinMode(PIN_INTER_RIGHT,OUTPUT);
+	pinMode(PIN_INTER_LEFT,OUTPUT);
+
 	//---===各参数清零===---//
 	motor_time=0;
 	motor_time_head=0;
@@ -122,39 +136,31 @@ void setup() {
 	memset(comdata, 0, 800);
 	memset(time_data, 0, 1);
 
-
 	//---===读取EEPROM中参数赋给当前坐标===---//
-	value_x_high=EEPROM.read(address_x_HIGH);
-	value_x_low=EEPROM.read(address_x_LOW);
-	value_y_high=EEPROM.read(address_y_HIGH);
-	value_y_low=EEPROM.read(address_y_LOW);
-	value_z_high=EEPROM.read(address_z_HIGH);
-	value_z_low=EEPROM.read(address_z_LOW);
+	value_high=EEPROM.read(ADDRESS_HIGH);
+	value_low=EEPROM.read(ADDRESS_LOW);
 
-	if((value_x_high==255)||(value_y_high==255)||(value_z_high=255))
+	if(value_high==255)
 	{
 		//初始化各地址初始值
-		EEPROM.write(address_x_HIGH, 0);
-		EEPROM.write(address_x_LOW, 0);
-		EEPROM.write(address_y_HIGH, 0);
-		EEPROM.write(address_y_LOW, 0);
-		EEPROM.write(address_z_HIGH, 0);
-		EEPROM.write(address_z_LOW, 0);
-		curpos_x=0;
-		curpos_y=0;
-		curpos_z=0;
+		EEPROM.write(ADDRESS_HIGH, 0);
+		EEPROM.write(ADDRESS_LOW, 0);
+		curpos_coor=0;
 	}
 	else
 	{
-		curpos_x=value_x_high*256+value_x_low;
-		curpos_y=value_y_high*256+value_y_low;
-		curpos_z=value_z_high*256+value_z_low;
+		curpos_coor=value_high*256+value_low;
 	}
 
 	//---===使能端有效===---//
-	pinMode(enable_x,LOW);
-	pinMode(enable_y,LOW);
-	pinMode(enable_z,LOW);
+	digitalWrite(PIN_ENABLE,LOW);
+
+	//---===限位开关置低===---//
+	digitalWrite(PIN_INTER_RIGHT,LOW);
+	digitalWrite(PIN_INTER_LEFT,LOW);
+
+	//中断开启
+	attachInterrupt(1,blink,RISING);
 
 	Serial.begin(9600);           //打开串口
 }
@@ -217,224 +223,170 @@ void printChar(unsigned char c)
 	Serial.print(c, HEX);
 	Serial.print(',');
 }
-void stop_motor()               //暂停
-{
-    pinMode(pin_x,LOW);
-    pinMode(pin_y,LOW);
-    pinMode(pin_z,LOW);
-    pinMode(enable_x,HIGH);
-    pinMode(enable_y,HIGH);
-    pinMode(enable_z,HIGH);
-}
-
 /*
- * 获得电机当前频率;
- * char sort：电机类别
- * int  n:需要的脉冲数; 
- * int & tf:频率;    
+ * 电机加速启动
+ * unsigned int & n;电机所需脉冲数
+ * int tf;电机所需延迟时间
 */
-
-void GetFreData(char sort,unsigned int n,int & tf)
+void Accelerate_Launch(unsigned int & n, int tf)
 {
-      int pulse_rate = 1;        //脉冲比值
-      int delay_lowlevenum_low = 0;          //电机低速低电平脉冲延迟数
-      int delay_lowlevenum_mid = 0;          //电机中速低电平脉冲延迟数
-      int delay_lowlevenum_hig = 0;          //电机高速低电平脉冲延迟数
-      switch(sort)
+      int d;
+      int acc_n;
+      acc_n=ACC_TIME * 2 / (tf + MAX_DELAY);
+      d = (MAX_DELAY - tf) / (acc_n - 1);
+      for(int i = MAX_DELAY; i > tf;)
       {
-        case 'x':                //x方向电机
-             pulse_rate = pulse_rate_x;
-             delay_lowlevenum_low = delay_lowlevenum_xlow;
-             delay_lowlevenum_mid = delay_lowlevenum_xmid;
-             delay_lowlevenum_hig = delay_lowlevenum_xhig;
-             break;
-        case 'y':             //y方向电机
-             pulse_rate=pulse_rate_y;
-             delay_lowlevenum_low = delay_lowlevenum_ylow;
-             delay_lowlevenum_mid = delay_lowlevenum_ymid;
-             delay_lowlevenum_hig = delay_lowlevenum_yhig;
-             break;
-        case 'z':             //z方向电机
-             pulse_rate = pulse_rate_y;
-             delay_lowlevenum_low = delay_lowlevenum_zlow;
-             delay_lowlevenum_mid = delay_lowlevenum_zmid;
-             delay_lowlevenum_hig = delay_lowlevenum_zhig;
-             break;
-        default:
-             Serial.println("ERROR:motor sort error！！！");
-             break;
-      }
-  
-      unsigned int x = n / pulse_rate;
-      if(x > 6)
-            tf = delay_lowlevenum_hig;
-      else if(x > 2)
-            tf = delay_lowlevenum_mid;
-      else
-            tf = delay_lowlevenum_low;
+            digitalWrite(PIN_PULS, LOW);
+            delay(i);
+            digitalWrite(PIN_PULS, HIGH);
+            delay(i);
+            i = i - d;
+            n--;
+      }     
 }
 
 /*
  * 电机驱动;
- * char sort：电机类别
  * int & n:需要的脉冲数; 
- * int & tf:频率;
- * int & count:低电平延迟数
- * boolean & flag:计数标志位
+ * int  tf:频率;
 */
-void motor(char sort,unsigned int & n,int & tf,int & count,boolean & flag)
+void motor(unsigned int  & n, int tf)
 {
-    int pin = 3;           //脉冲端口
-    if(n==0)
+    //电机加速启动
+//    Accelerate_Launch(n, tf);
+    //电机转动
+    for(; n > 0 && inter_stop == false; n--)
     {
-        flag=true;
-    }
-    switch(sort)
-    {
-      case 'x':                //x方向电机
-           pin=pin_x;
-           break;
-      case 'y':              //y方向电机
-           pin=pin_y;
-           break;
-      case 'z':             //z方向电机
-           pin=pin_z;
-           break;
-      default:
-           Serial.println("ERROR:motor sort error！！！");
-           break;
-    }
-    
-    if(flag==false)
-    {
-        if(count==tf)
-        {
-            digitalWrite(pin,HIGH);
-            count=0;
-            n--;
-            //获得下一脉冲所需脉冲下的电机低电平延迟数
-            GetFreData(sort,n,tf);
-        }
-        else
-        {
-            digitalWrite(pin,LOW);
-            count++;
-        }
+          digitalWrite(PIN_PULS, LOW);
+          delayMicroseconds(tf);
+          digitalWrite(PIN_PULS, HIGH);
+          delayMicroseconds(tf);
     }
 }
 
 /*
  * 运动规划；
- * int s_x:x方向所需运动位移；
- * int s_y:y方向所需运动位移；
- * int s_z:z方向所需运动位移；
+ * int distance_x:x方向所需运动位移；
+ * int distance_y:y方向所需运动位移；
+ * int distance_z:z方向所需运动位移；
 */
-void dmotor(int s_x,int s_y,int s_z)
+void dmotor(int distance_x, int distance_y, int distance_z)
 {
      //定义基本变量
-     int tf_x,tf_y,tf_z;
-     int s_x_abs=0,s_y_abs=0,s_z_abs=0;  
-     int count_x=0,count_y=0,count_z=0;                  //x，y，z方向电机计数器
-     boolean flag_x=false,flag_y=false,flag_z=false;                //x，y，z方向电机完成任务信号标志
+     int delay_tf;
+     int tran_ration;
+     int pulse_rate;
+     int distance_abs;  
+     char sort;
+
+     
+     sort = SORT;
+     //根据类型确定基本变量
+     switch(sort)
+     {
+          case 'x':
+               distance_coor = distance_x;
+               delay_tf = DELAY_TF_X;
+               tran_ration = TRAN_RATION_X;
+               pulse_rate = PULSE_RATE_X;
+               break;
+          case 'y':
+	       Serial.println("I'm Y AXIS");
+               distance_coor = distance_y;
+               delay_tf = DELAY_TF_Y;
+               tran_ration = TRAN_RATION_Y;
+               pulse_rate = PULSE_RATE_Y;
+               break;
+          case 'z':
+               distance_coor = distance_z;
+               delay_tf = DELAY_TF_Z;
+               tran_ration = TRAN_RATION_Z;
+               pulse_rate = PULSE_RATE_Z;
+               break;
+          default:
+           Serial.println("ERROR:motor sort error！！！");
+           stop_motor();
+           break;
+     }
+
      //使能端开启有效
-     pinMode(enable_x,LOW);
-     pinMode(enable_y,LOW);
-     pinMode(enable_z,LOW);
+     digitalWrite(PIN_ENABLE,LOW);
+     
+      //判断向电机转动方向
+     if(distance_coor < 0)                           //x方向电机反转 同方向左转
+     {
+         int val = digitalRead(PIN_INTER_RIGHT);
+         if(val)
+         {
+             stop_motor();
+             return;
+         }
+         digitalWrite(PIN_DIR,LOW);
+         distance_abs = abs(distance_coor);
+     }
+     else
+     {
+         int val = digitalRead(PIN_INTER_LEFT);
+         if(val)
+         {
+             stop_motor();
+             return;
+         }
+         digitalWrite(PIN_DIR,HIGH);
+         distance_abs = abs(distance_coor);
+     }    
     
-     //判断x，y，z方向电机转动方向
-     if(s_x<0)                           //x方向电机反转
-     {
-         digitalWrite(dir_x,LOW);
-         s_x_abs=abs(s_x);
-     }
-     if(s_x>0)                          //x方向电机正转
-     {
-         digitalWrite(dir_x,HIGH);
-         s_x_abs=abs(s_x);
-     }
-     
-     if(s_y<0)                           //y方向电机反转
-     {
-         digitalWrite(dir_y,LOW);
-         s_y_abs=abs(s_y);
-     }
-     if(s_y>0)                          //y方向电机正转
-     {
-         digitalWrite(dir_y,HIGH);
-         s_y_abs=abs(s_y);
-     }
-     
-     if(s_z<0)                           //x方向电机反转
-     {
-         digitalWrite(dir_z,LOW);
-         s_z_abs=abs(s_z);
-     }
-     if(s_z>0)                          //x方向电机正转
-     {
-         digitalWrite(dir_z,HIGH);
-         s_z_abs=abs(s_z);
-     }
 
      //将距离转换为脉冲数
-     unsigned int pulse_x=int(s_x_abs * (800.0 / DISSWANG_X));
-     unsigned int pulse_y=int(s_y_abs * (220.0 / DISSWANG_Y));
-     unsigned int pulse_z=int(s_z_abs * (400.0 / DISSWANG_Z));
+     unsigned int puls_add = int(distance_abs * ( pulse_rate / (tran_ration * 1.0)));
 
-     //分别获得x,y,z方向频率
-     GetFreData('x',pulse_x,tf_x);
-     GetFreData('y',pulse_y,tf_y);
-     GetFreData('z',pulse_z,tf_z);
+     
+     Serial.println(puls_add, DEC);
+     
+     //保存此过程所需脉冲数
+     unsigned int keep_add = puls_add;
 
      motor_time_end=millis();                       //开始记录时间
-     while(flag_x==false||flag_y==false||flag_z==false)
-     {
-          motor('x',pulse_x,tf_x,count_x,flag_x);
-          motor('y',pulse_y,tf_y,count_y,flag_y);
-          motor('z',pulse_z,tf_z,count_z,flag_z);
-     }
+     
+     Serial.print("puls_add:");
+     Serial.println(puls_add, DEC);
+     Serial.print("delay_tf:");
+     Serial.println(delay_tf, DEC);
+
+     motor(puls_add, delay_tf); 
+     
      motor_time_head=millis();                    //记录运行结束时间
      motor_time=motor_time_head-motor_time_end;  //获得运行时间
 
+     //求出当前位置
+     curpos_coor = curpos_coor + distance_coor * (1 - puls_add / keep_add);
+     
       //将当前位置保存至EEPROM中
-      value_time_0=curpos_x%256;
-      value_time_1=(curpos_x/256)%256;
-      value_time_2=(curpos_x/256/256)%256;
-      value_time_3=(curpos_x/256/256/256)%256;
+      value_time_0 = motor_time%256;
+      value_time_1 = (motor_time/256)%256;
+      value_time_2 = (motor_time/256/256)%256;
+      value_time_3 = (motor_time/256/256/256)%256;
 
       EEPROM.write(address_time_0, value_time_0);
       EEPROM.write(address_time_1, value_time_1);
       EEPROM.write(address_time_2, value_time_2);
       EEPROM.write(address_time_3, value_time_3);
-      
-     //将目标位置赋值给当前位置
-     curpos_x=objpos_x;
-     curpos_y=objpos_y;
-     curpos_z=objpos_z;
 
      //将当前位置保存至EEPROM中
-    value_x_high=curpos_x/256;
-    value_x_low=curpos_x%256;
-    value_y_high=curpos_x/256;
-    value_y_low=curpos_x%256;
-    value_z_high=curpos_x/256;
-    value_z_low=curpos_x%256;
+     value_high = curpos_coor/256;
+     value_low  = curpos_coor%256;
 
-    EEPROM.write(address_x_HIGH, value_x_high);
-    EEPROM.write(address_x_LOW, value_x_low);
-    EEPROM.write(address_y_HIGH, value_y_high);
-    EEPROM.write(address_y_LOW, value_y_low);
-    EEPROM.write(address_z_HIGH, value_z_high);
-    EEPROM.write(address_z_LOW, value_z_low);
-     
-     //标志位复位
-     flag_x=false;
-     flag_y=false;
-     flag_z=false;
-
+     EEPROM.write(ADDRESS_HIGH, value_high);
+     EEPROM.write(ADDRESS_LOW, value_low);
 }
 
 void processMotor()
 {
+	int objpos_x = 0, objpos_y = 0, objpos_z = 0;           //目标位置坐标
+	int curpos_x = 0, curpos_y = 0, curpos_z = 0;          //当前目标坐标
+	int distance_x = 0, distance_y = 0, distance_z = 0;   //所需移动位移
+        
+        inter_stop = false;
 	switch(cmd.data.mode)
 	{
 		case 'A':                       //move:输入的x,y,z是绝对坐标
@@ -450,6 +402,8 @@ void processMotor()
 			Serial.println("Here A !!!!!");
 			break;
 		case 'B':                     //move:输入的x,y,z是相对坐标
+			Serial.println("Here B moving!!!");
+
 			distance_x=para[0].d;
 			distance_y=para[1].d;
 			distance_z=para[2].d;
@@ -458,7 +412,8 @@ void processMotor()
 			objpos_z=distance_z+curpos_z;               //所需运行位移
 
 			dmotor(distance_x,distance_y,distance_z);       //电机驱动
-			Serial.println("Here B !!!!!");
+
+			Serial.println("Finished B moving!!!");
 			break;
 		case 'C':                     //stop:电机停止
 			void stop_motor(); 
