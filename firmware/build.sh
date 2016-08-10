38ed57d5a0bf17a6f03b1f532e966846bc9234f1
Sun, Yi <sunyi@xiaokr.cc>
2016-08-10 09:09:35 +0000

    Add build.sh which can upload firmware to axis X,Y,Z one by one.
    Signed-off-by: Sun, Yi <sunyi@xiaokr.cc>

---
+++ firmware/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 0/ firmware.ino 
+make ARDUINO_PORT=/dev/ArduinoX upload
+
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 1/ firmware.ino 
+make ARDUINO_PORT=/dev/ArduinoY upload
+
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 2/ firmware.ino 
+make ARDUINO_PORT=/dev/ArduinoZ upload
