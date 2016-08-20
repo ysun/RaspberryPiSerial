27eb2abde9d4b3fab2ddae53fdcbc180343b33dc
Sun, Yi <sunyi@xiaokr.cc>
2016-08-20 01:55:30 +0000

    Refine build script for adding /dev/ArduinoW support.
    Signed-off-by: Sun, Yi <sunyi@xiaokr.cc>

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 0/ firmware.ino 
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 0/ firmware.ino
 make ARDUINO_PORT=/dev/ArduinoX upload
 
-sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 1/ firmware.ino 
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 1/ firmware.ino
 make ARDUINO_PORT=/dev/ArduinoY upload
 
-sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 2/ firmware.ino 
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 2/ firmware.ino
 make ARDUINO_PORT=/dev/ArduinoZ upload
+
+sed  -i -e s/#define\ AXIS.*/#define\ AXIS\ 3/ firmware.ino
+make ARDUINO_PORT=/dev/ArduinoW upload
